e010e2b8212240fca860c4f56c0036d5e38bff86
Juan Gonon <juanfegonon27@gmail.com>
2024-06-04 11:49:38 -0600

    context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { GoogleAuthProvider, signInWithRedirect, signOut, onAuthStateChanged } from 'firebase/auth/web-extension'
+import { auth } from '../api/firebase.config'
+
+export const AuthContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({})
+
+  const googleSingIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
+  }
+
+  const logOut = () => {
+    signOut(auth)
+  }
+
+  useEffect(() => {
+    const unSubCribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+    })
+
+    return () => unSubCribe()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{
+      user,
+      googleSingIn,
+      logOut
+    }}
+    >
+      {
+        children
+    }
+    </AuthContext.Provider>
+  )
+}
+
+export const UserAuth = () => {
+  return useContext(AuthContext)
+}
