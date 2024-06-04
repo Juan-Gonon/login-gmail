e78b4b38448afa5f529afb6932945957edd11043
Juan Gonon <juanfegonon27@gmail.com>
2024-06-04 11:29:16 -0600

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import styled from 'styled-components'
 import './App.css'
 
 function App () {
   return (
-    <>
+    <Container>
       <h1>LOGIN GMAIL</h1>
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  max-width: 100vw;
+  max-height: 100vh;
+  display: flex;
+`
 
 export default App
